a44d3e3ed4dd28fb946d5b408f68cbba66300ef2
Vicky Sharma <136226399+Vicky3196@users.noreply.github.com>
2023-06-17 23:06:15 +0530

    Update Mobile_Tracking.py
    Mobile Tracking
    
    #  Mobile phone tracking is a process for identifying the location of a mobile phone, 
       whether stationary or moving.
    #  Phone numbers is one of the modules that provides numerous Features like providing 
       basic information about a phone number, validation of a phone number, etc.
    #  The phone number is entered and a library is used to turn the country calling code. For 
       example numbers starting with +91 becomes India, +880 is Bangladesh, +34 is Spain, 
       etc.

--- Mobile_Tracking.py
+++ Mobile_Tracking.py
@@ -1,14 +1,3 @@
-Mobile Tracking
-
-#  Mobile phone tracking is a process for identifying the location of a mobile phone, 
-   whether stationary or moving.
-#  Phone numbers is one of the modules that provides numerous Features like providing 
-   basic information about a phone number, validation of a phone number, etc.
-#  The phone number is entered and a library is used to turn the country calling code. For 
-   example numbers starting with +91 becomes India, +880 is Bangladesh, +34 is Spain, 
-   etc.
-
-
 import phonenumbers 
 from phonenumbers import timezone,geocoder,carrier
 number = input("Enter your no. with +__: ")
